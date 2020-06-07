f3e4761e79b34bc61a7bca420638fccb7dd59075
Alvin Konda <alvinkonda@gmail.com>
2020-06-08 00:24:10 +0200

    fixed classes for support cookies and javascript

--- assets/theme.js
+++ assets/theme.js
@@ -1,11 +1,13 @@
 $(function() {
 
   // Detect that Javascript is enabled
-  $("html").removeClass("no-js")
+  $("html").removeClass("no-js").addClass('js')
 
   // Detect that Cookies are enabled
-  if (navigator.cookieEnabled)
-    $("html").removeClass("supports-no-cookies")
+  if (navigator.cookieEnabled) {
+    $(".supports-cookies").removeClass("d-none")
+    $(".supports-no-cookies").addClass("d-none")
+  }
 
   // Initialize Bootstrap Tooltips
   $('[data-toggle="tooltip"]').tooltip()
