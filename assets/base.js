e1d27e652e58f0e7479d35b2be596a7d21c97248
Alvin Konda <alvinkonda@gmail.com>
2021-06-22 00:32:05 +0200

    admin

---
+++ assets/base.js
@@ -0,0 +1,39 @@
+/*
+    © 2021 Firetheme.com
+*/
+
+//
+// Init Bootstrap tooltips
+//
+document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    .forEach((el) => new bootstrap.Tooltip(el));
+
+//
+// Shopify's callenge page - Add BS classes
+//
+(() => {
+    const btn = document.querySelector('.btn.shopify-challenge__button')
+    if (btn) {
+        btn.classList.add('btn-primary')
+    }
+})();
+
+//
+// Shopify's errors message - Add BS classes
+//
+(() => {
+    const errors = document.querySelector('.errors')
+    if (errors) {
+        errors.classList.add('alert', 'alert-danger')
+        errors.querySelector('ul').classList.add('mb-0')
+    }
+})()
+
+//
+// Newsletter - Scroll to section after submit
+//
+if (window.location.href.indexOf('?customer_posted=true') > -1) {
+    setTimeout(() => {
+        document.querySelector('.newsletter-alert').scrollIntoView()
+    }, 750)
+}
--- assets/init.js
+++
@@ -1,9 +0,0 @@
-/*
-    © 2021 Firetheme.com
-*/
-
-//
-// Bootstrap tooltips
-//
-document.querySelectorAll('[data-bs-toggle="tooltip"]')
-    .forEach((el) => new bootstrap.Tooltip(el))
