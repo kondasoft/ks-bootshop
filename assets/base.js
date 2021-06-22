c54ab59634b8a40d27c66daf5b74440c1f8acc9b
Alvin Konda <alvinkonda@gmail.com>
2021-06-22 12:44:28 +0200

    admin

--- assets/base.js
+++ assets/base.js
@@ -37,3 +37,15 @@ if (window.location.href.indexOf('?customer_posted=true') > -1) {
         document.querySelector('.newsletter-alert').scrollIntoView()
     }, 750)
 }
+
+//
+// Blog - Filter by
+//
+document.querySelector('#blog-filter-by')?.addEventListener('change', (e) => {
+    const params = window.location.href.split('?')[1]
+    if (params) {
+        window.location.href = `${e.target.value}?${params}`
+    } else {
+        window.location.href = e.target.value
+    }
+})
